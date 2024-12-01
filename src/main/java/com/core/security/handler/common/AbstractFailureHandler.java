e412f2a20b7d631444b85921feb9d60a7a12ab0a
RosieOh <20172207@gm.hannam.ac.kr>
2024-12-01 17:56:07 +0900

    FEAT : ErrorCode에 따른 응답 헤더(콘텐츠 타입 및 상태 코드) 설정 로직 추가

---
+++ src/main/java/com/core/security/handler/common/AbstractFailureHandler.java
@@ -0,0 +1,55 @@
+package com.core.security.handler.common;
+
+import com.core.core.dto.ExceptionDto;
+import com.core.core.exception.error.ErrorCode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public abstract class AbstractFailureHandler {
+
+    private static final String CONTENT_TYPE = "application/json";
+    private static final String CHARACTER_ENCODING = "UTF-8";
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    protected void setErrorResponse(
+            HttpServletResponse response,
+            ErrorCode errorCode
+    ) throws IOException {
+        setResponseHeader(response, errorCode);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", false);
+        result.put("data", null);
+        result.put("error", ExceptionDto.of(errorCode));
+
+        response.getWriter().write(objectMapper.writeValueAsString(result));
+    }
+
+    protected void setErrorResponse(
+            HttpServletResponse response,
+            String message,
+            ErrorCode errorCode
+    ) throws IOException {
+        setResponseHeader(response, errorCode);
+
+        Map<String, Object> result = new HashMap<>();
+        result.put("success", false);
+        result.put("data", null);
+        result.put("error", ExceptionDto.of(errorCode, message));
+
+        response.getWriter().write(objectMapper.writeValueAsString(result));
+    }
+    
+    private void setResponseHeader(
+            HttpServletResponse response,
+            ErrorCode errorCode
+    ) {
+        response.setContentType(CONTENT_TYPE);
+        response.setCharacterEncoding(CHARACTER_ENCODING);
+        response.setStatus(errorCode.getHttpStatus().value());
+    }
+}
